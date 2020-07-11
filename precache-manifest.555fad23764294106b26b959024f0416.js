740d743ee125c70fa2711c722e2bd2dc0ca42c30
ArkanFauzan <35446583+ArkanFauzan@users.noreply.github.com>
2020-07-11 20:05:39 +0700

    tes

---
+++ precache-manifest.555fad23764294106b26b959024f0416.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4f95de99339dd1776fb88a1a9686c6a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c2ad0c9fecb7b986fe02",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "1934034cc5a381f975ba",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c2ad0c9fecb7b986fe02",
+    "url": "/static/js/2.18556aee.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.18556aee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1934034cc5a381f975ba",
+    "url": "/static/js/main.fc296bbd.chunk.js"
+  },
+  {
+    "revision": "a80313dc47a88ed0f3b7",
+    "url": "/static/js/runtime-main.9a1746e8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
